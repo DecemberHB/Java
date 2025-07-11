f5073e7169fe792775a8354207b9152226a5ec72
27hyobin <27hyobin@gmail.com>
2025-07-11 17:20:33 +0900

    250711ver

--- CodigTest/src/step3/Sample11.java
+++ CodigTest/src/step3/Sample11.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 public class Sample11 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
+		
 	
+		while(true) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			int sum = A+B;
+			
+
+			if (A==0&&B==0) {
+				break;
+			}
+			System.out.println(sum);
+
+		}
 
 	}
 }
--- ch03/src/sub3/ForTest.java
+++ ch03/src/sub3/ForTest.java
@@ -68,6 +68,7 @@ public class ForTest {
 			System.out.print("\n");
 		}
 		
+		
 	}
 
 }
